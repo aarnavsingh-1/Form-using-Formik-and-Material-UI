5ff0ef0185cffbe6d896fddf062a86ed607ce597
Aarnav <aarnav753@gmail.com>
2023-06-30 19:43:15 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { TextField, Select, MenuItem, Button } from '@mui/material';
+import * as yup from 'yup';
 
-function App() {
+const validationSchema = yup.object().shape({
+  name: yup.string().required('Name is required'),
+  address: yup.string().required('Address is required'),
+  country: yup.string().required('Country is required'),
+});
+
+const initialValues = {
+  name: '',
+  address: '',
+  country: '',
+};
+
+const countries = [
+  { value: 'usa', label: 'USA' },
+  { value: 'canada', label: 'Canada' },
+  { value: 'uk', label: 'UK' },
+  { value: 'australia', label: 'Australia' },
+];
+
+const App = () => {
+  const handleSubmit = (values, { setSubmitting,resetForm }) => {
+    console.log(values);
+    resetForm();
+    setSubmitting(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>User Information Form</h1>
+     <div style={{ margin: '20px' }}>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <div>
+              <Field as={TextField} label="Name" name="name" fullWidth />
+              <ErrorMessage name="name" component="div" />
+            </div>
+            <div>
+              <Field as={TextField} label="Address" name="address" fullWidth multiline rows={4} />
+              <ErrorMessage name="address" component="div" />
+            </div>
+            <div>
+              <Field as={Select} label="Country" name="country" fullWidth>
+                <MenuItem value="" disabled>
+                  Select a country
+                </MenuItem>
+                {countries.map((country) => (
+                  <MenuItem key={country.value} value={country.value}>
+                    {country.label}
+                  </MenuItem>
+                ))}
+              </Field>
+              <ErrorMessage name="country" component="div" />
+            </div>
+            <Button type="submit" variant="contained"disabled={isSubmitting}>Submit</Button>
+          </Form>
+        )}
+      </Formik>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
